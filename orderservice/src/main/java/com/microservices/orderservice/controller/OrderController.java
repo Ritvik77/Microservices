ac21a6dc82a727a33bd5cd7cad2a7ddae69d87fb
Ritvik77 <ritvikroshan.s2019@vitstudent.ac.in>
2024-11-19 08:15:37 +0530

    added feignclient for order service to connect with inventory

--- orderservice/src/main/java/com/microservices/orderservice/controller/OrderController.java
+++ orderservice/src/main/java/com/microservices/orderservice/controller/OrderController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.microservices.orderservice.model.Order;
 import com.microservices.orderservice.request.OrderRequest;
 import com.microservices.orderservice.service.OrderService;
 
@@ -20,9 +21,8 @@ public class OrderController {
 
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public String saveOrder(@RequestBody OrderRequest orderRequest) {
-		orderService.placeOrder(orderRequest);
-		return "order placed Successfully";
+	public Order saveOrder(@RequestBody OrderRequest orderRequest) {
+		return orderService.placeOrder(orderRequest);
 	}
 
 }
--- orderservice/src/main/java/com/microservices/orderservice/service/OrderService.java
+++ orderservice/src/main/java/com/microservices/orderservice/service/OrderService.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.microservices.orderservice.client.InventoryClient;
 import com.microservices.orderservice.model.Order;
 import com.microservices.orderservice.repository.OrderRepository;
 import com.microservices.orderservice.request.OrderRequest;
@@ -15,12 +16,23 @@ public class OrderService {
 	@Autowired
 	private OrderRepository orderRepository;
 
-	public void placeOrder(OrderRequest orderRequest) {
-		Order order = new Order();
-		order.setOrderNumber(UUID.randomUUID().toString());
-		order.setPrice(orderRequest.price());
-		order.setSkuCode(orderRequest.skucode());
-		order.setQuantity(orderRequest.quantity());
-		orderRepository.save(order);
+	@Autowired
+	private InventoryClient inventoryClient;
+
+	public Order placeOrder(OrderRequest orderRequest) {
+
+		var isAvailable = inventoryClient.isInStock(orderRequest.skucode(), orderRequest.quantity());
+
+		if (isAvailable) {
+			Order order = new Order();
+			order.setOrderNumber(UUID.randomUUID().toString());
+			order.setPrice(orderRequest.price());
+			order.setSkuCode(orderRequest.skucode());
+			order.setQuantity(orderRequest.quantity());
+			orderRepository.save(order);
+			return order;
+		} else {
+			throw new RuntimeException("Product with skuCode: " + orderRequest.skucode() + " is not available ");
+		}
 	}
 }
